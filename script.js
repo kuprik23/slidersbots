d88050841f67b0a9d305f4709dab9636295b48b1
kuprik23 <martkrupik@gmail.com>
2023-08-24 00:28:39 +0100

    changepatch

--- script.js
+++ script.js
@@ -1,103 +1,91 @@
-// Get references to the sliders, code display div, and close button
-const slider1 = document.getElementById('slider1');
-const slider2 = document.getElementById('slider2');
-const slider3 = document.getElementById('slider3');
-const generateButton = document.getElementById('generateButton');
+// Get references to elements
+const cubeContainer = document.getElementById('cube-container');
+const generateCodeButton = document.getElementById('generateCodeButton');
 const codeDisplay = document.getElementById('codeDisplay');
 const codeOutput = document.getElementById('codeOutput');
-const closeCodeDisplay = document.getElementById('closeCodeDisplay');
-const mouth = document.getElementById('mouth');
-// Get references to elements
-const avatarContainer = document.getElementById('avatar-container');
-const toggleAvatarButton = document.getElementById('toggleAvatarButton');
-const slider1 = document.getElementById('slider1');
-const slider2 = document.getElementById('slider2');
-const slider3 = document.getElementById('slider3');
+const sliders = [document.getElementById('slider1'), document.getElementById('slider2'), document.getElementById('slider3')];
 
-// Set up the scene
+// Create a scene
 const scene = new THREE.Scene();
+
+// Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-avatarContainer.appendChild(renderer.domElement);
-
-// Create a simple cube as an example of an avatar (replace with your FBX model)
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const avatar = new THREE.Mesh(geometry, material);
-scene.add(avatar);
-
-// Camera position
 camera.position.z = 5;
 
-// Variable to keep track of the avatar visibility
-let isAvatarVisible = true;
+// Create a renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(300, 300);
+cubeContainer.appendChild(renderer.domElement);
 
-// Function to toggle the avatar visibility
-function toggleAvatar() {
-    isAvatarVisible = !isAvatarVisible;
-    avatar.visible = isAvatarVisible;
+// Create cubes with different colors
+const cubeMaterials = [
+    new THREE.MeshBasicMaterial({ color: 0xff0000 }), // Red
+    new THREE.MeshBasicMaterial({ color: 0x0000ff }), // Blue
+    new THREE.MeshBasicMaterial({ color: 0xffa500 })  // Orange
+];
+
+const cubes = [];
+for (let i = 0; i < cubeMaterials.length; i++) {
+    const geometry = new THREE.BoxGeometry();
+    const cube = new THREE.Mesh(geometry, cubeMaterials[i]);
+    cube.position.x = i * 2.5;
+    scene.add(cube);
+    cubes.push(cube);
 }
 
-// Event listener for the toggle button
-toggleAvatarButton.addEventListener('click', toggleAvatar);
+// Function to update the cube's color based on slider values
+function updateCubeColor() {
+    const red = sliders[0].value;
+    const green = sliders[1].value;
+    const blue = sliders[2].value;
 
-// Function to update the avatar based on slider values
-function updateAvatar() {
-    // Example: Update avatar properties based on slider values
-    const personalityTrait1 = slider1.value;
-    const personalityTrait2 = slider2.value;
-    const personalityTrait3 = slider3.value;
-
-    // Modify the avatar based on slider values
-    // Example: avatar.scale.set(personalityTrait1 / 100, personalityTrait2 / 100, personalityTrait3 / 100);
+    cubes.forEach(cube => {
+        cube.material.color.setRGB(red / 255, green / 255, blue / 255);
+    });
 }
 
 // Event listeners for the sliders
-slider1.addEventListener('input', updateAvatar);
-slider2.addEventListener('input', updateAvatar);
-slider3.addEventListener('input', updateAvatar);
+sliders.forEach(slider => {
+    slider.addEventListener('input', updateCubeColor);
+});
 
-// Animation loop
-const animate = () => {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-};
+// Function to generate code based on the cube's color
+function generateCode() {
+    const red = sliders[0].value;
+    const green = sliders[1].value;
+    const blue = sliders[2].value;
 
-animate();
-
-// Function to generate Three.js code and open the mouth
-function generateThreeJsCode() {
-    const personalityTrait1 = slider1.value;
-    const personalityTrait2 = slider2.value;
-    const personalityTrait3 = slider3.value;
-
-    // Generate Three.js code based on slider values
     const generatedCode = `
-        // Your Three.js code here
-        const avatar = new Avatar();
-        avatar.setPersonality(${personalityTrait1}, ${personalityTrait2}, ${personalityTrait3});
-        // ...
+        // Create a cube with the selected color
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x${rgbToHex(red, green, blue)} });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
     `;
 
     // Display the generated code in the codeOutput element
     codeOutput.textContent = generatedCode;
 
-    // Open the mouth (add the 'open-mouth' class)
-    mouth.classList.add('open-mouth');
-
     // Show the code display div
     codeDisplay.style.display = 'block';
 }
 
-// Event listener for the generate button
-generateButton.addEventListener('click', generateThreeJsCode);
+// Event listener for the generate code button
+generateCodeButton.addEventListener('click', generateCode);
 
-// Event listener to close the code display and close the mouth
-closeCodeDisplay.addEventListener('click', () => {
-    codeDisplay.style.display = 'none';
+// Animation loop
+const animate = () => {
+    requestAnimationFrame(animate);
+    cubes.forEach(cube => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    });
+    renderer.render(scene, camera);
+};
 
-    // Close the mouth (remove the 'open-mouth' class)
-    mouth.classList.remove('open-mouth');
-});
+animate();
+
+// Utility function to convert RGB to hex
+function rgbToHex(r, g, b) {
+    return ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+}

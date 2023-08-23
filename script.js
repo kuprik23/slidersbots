3d4ead0ad570479189d63fb9b14365ef2ae4b2f7
kuprik23 <martkrupik@gmail.com>
2023-08-24 00:18:23 +0100

    change

--- script.js
+++ script.js
@@ -7,6 +7,65 @@ const codeDisplay = document.getElementById('codeDisplay');
 const codeOutput = document.getElementById('codeOutput');
 const closeCodeDisplay = document.getElementById('closeCodeDisplay');
 const mouth = document.getElementById('mouth');
+// Get references to elements
+const avatarContainer = document.getElementById('avatar-container');
+const toggleAvatarButton = document.getElementById('toggleAvatarButton');
+const slider1 = document.getElementById('slider1');
+const slider2 = document.getElementById('slider2');
+const slider3 = document.getElementById('slider3');
+
+// Set up the scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+avatarContainer.appendChild(renderer.domElement);
+
+// Create a simple cube as an example of an avatar (replace with your FBX model)
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const avatar = new THREE.Mesh(geometry, material);
+scene.add(avatar);
+
+// Camera position
+camera.position.z = 5;
+
+// Variable to keep track of the avatar visibility
+let isAvatarVisible = true;
+
+// Function to toggle the avatar visibility
+function toggleAvatar() {
+    isAvatarVisible = !isAvatarVisible;
+    avatar.visible = isAvatarVisible;
+}
+
+// Event listener for the toggle button
+toggleAvatarButton.addEventListener('click', toggleAvatar);
+
+// Function to update the avatar based on slider values
+function updateAvatar() {
+    // Example: Update avatar properties based on slider values
+    const personalityTrait1 = slider1.value;
+    const personalityTrait2 = slider2.value;
+    const personalityTrait3 = slider3.value;
+
+    // Modify the avatar based on slider values
+    // Example: avatar.scale.set(personalityTrait1 / 100, personalityTrait2 / 100, personalityTrait3 / 100);
+}
+
+// Event listeners for the sliders
+slider1.addEventListener('input', updateAvatar);
+slider2.addEventListener('input', updateAvatar);
+slider3.addEventListener('input', updateAvatar);
+
+// Animation loop
+const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+
+animate();
 
 // Function to generate Three.js code and open the mouth
 function generateThreeJsCode() {

923c80dd3928b833bb776af4a345ebad185c8758
kuprik23 <martkrupik@gmail.com>
2023-08-23 23:40:50 +0100

    init

---
+++ script.js
@@ -0,0 +1,39 @@
+// Add your JavaScript code here
+
+// Get references to the sliders, code display div, and close button
+const slider1 = document.getElementById('slider1');
+const slider2 = document.getElementById('slider2');
+const slider3 = document.getElementById('slider3');
+const generateButton = document.getElementById('generateButton');
+const codeDisplay = document.getElementById('codeDisplay');
+const codeOutput = document.getElementById('codeOutput');
+const closeCodeDisplay = document.getElementById('closeCodeDisplay');
+
+// Function to generate Three.js code and display it
+function generateThreeJsCode() {
+    const personalityTrait1 = slider1.value;
+    const personalityTrait2 = slider2.value;
+    const personalityTrait3 = slider3.value;
+
+    // Generate Three.js code based on slider values
+    const generatedCode = `
+        // Your Three.js code here
+        const avatar = new Avatar();
+        avatar.setPersonality(${personalityTrait1}, ${personalityTrait2}, ${personalityTrait3});
+        // ...
+    `;
+
+    // Display the generated code in the codeOutput element
+    codeOutput.textContent = generatedCode;
+
+    // Show the code display div
+    codeDisplay.style.display = 'block';
+}
+
+// Event listener for the generate button
+generateButton.addEventListener('click', generateThreeJsCode);
+
+// Event listener to close the code display
+closeCodeDisplay.addEventListener('click', () => {
+    codeDisplay.style.display = 'none';
+});

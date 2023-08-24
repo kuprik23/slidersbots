c257e24061dcaeb5ef2af399d5d01a55dc00ee51
kuprik23 <martkrupik@gmail.com>
2023-08-24 22:09:31 +0100

    js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const blueSlider = document.getElementById("blue-slider");
     const generateButton = document.getElementById("generate-button");
     const generatedCode = document.getElementById("generated-code");
+    const personalitySlider1 = document.getElementById("personality-slider-1");
+    const personalitySlider2 = document.getElementById("personality-slider-2");
+    const personalitySlider3 = document.getElementById("personality-slider-3");
 
     // Function to update the color of the cube based on slider values
     function updateCubeColor() {
@@ -21,21 +24,33 @@ document.addEventListener("DOMContentLoaded", function () {
     greenSlider.addEventListener("input", updateCubeColor);
     blueSlider.addEventListener("input", updateCubeColor);
 
-    // Event listener for generating random code
-    generateButton.addEventListener("click", function () {
-        const randomCode = generateRandomCode();
-        generatedCode.textContent = randomCode;
-    });
+    // Function to generate real code based on slider values and personality
+    function generateRealCode() {
+        const redValue = redSlider.value;
+        const greenValue = greenSlider.value;
+        const blueValue = blueSlider.value;
 
-    // Function to generate random code
-    function generateRandomCode() {
-        // Replace this with your code generation logic
-        const code = `// Your generated code:
-        function myFunction() {
-            console.log("Hello, EMERSA!");
+        const personalityTrait1 = personalitySlider1.value;
+        const personalityTrait2 = personalitySlider2.value;
+        const personalityTrait3 = personalitySlider3.value;
+
+        // Replace this with your actual code generation logic
+        const code = `
+        // Your generated code:
+        function customizeCube() {
+            const cube = new Cube();
+            cube.setColor(${redValue}, ${greenValue}, ${blueValue});
+            cube.setPersonality(${personalityTrait1}, ${personalityTrait2}, ${personalityTrait3});
+            return cube;
         }
-        myFunction();`;
+        `;
         
         return code;
     }
+
+    // Event listener for generating real code
+    generateButton.addEventListener("click", function () {
+        const realCode = generateRealCode();
+        generatedCode.textContent = realCode;
+    });
 });

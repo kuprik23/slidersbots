43d28032df53cb2123a2b0efe0f9169e421f520b
kuprik23 <martkrupik@gmail.com>
2023-08-24 22:05:07 +0100

    chng5

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Initialize your Three.js scene here
-
-    // Event listeners for sliders, buttons, and code generation
+    const colorChangeCube = document.getElementById("color-change-cube");
     const redSlider = document.getElementById("red-slider");
     const greenSlider = document.getElementById("green-slider");
     const blueSlider = document.getElementById("blue-slider");
-    const personalitySlider1 = document.getElementById("personality-slider-1");
-    const personalitySlider2 = document.getElementById("personality-slider-2");
-    const personalitySlider3 = document.getElementById("personality-slider-3");
     const generateButton = document.getElementById("generate-button");
-    const languageSelect = document.getElementById("language-select");
-    const formatSelect = document.getElementById("format-select");
-    const codeWindow = document.getElementById("code-window");
+    const generatedCode = document.getElementById("generated-code");
 
-    // Initialize your Three.js scene and objects here
-    // ...
+    // Function to update the color of the cube based on slider values
+    function updateCubeColor() {
+        const redValue = redSlider.value;
+        const greenValue = greenSlider.value;
+        const blueValue = blueSlider.value;
 
-    // Event listener for Generate Code button
-    generateButton.addEventListener("click", function () {
-        // Get the selected language and format
-        const selectedLanguage = languageSelect.value;
-        const selectedFormat = formatSelect.value;
-
-        // Generate code based on selected options
-        const generatedCode = generateCode(selectedLanguage, selectedFormat);
-
-        // Display the generated code in the code window
-        codeWindow.textContent = generatedCode;
-    });
-
-    // Function to generate code based on selected options
-    function generateCode(language, format) {
-        // Implement code generation logic based on language and format
-        // ...
-
-        // For demonstration, we'll return a placeholder message
-        return `Generated code (${language}, ${format}) will be displayed here.`;
+        const color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+        colorChangeCube.style.backgroundColor = color;
     }
 
-    // Additional functionality for 3D rendering, sliders, etc.
-    // ...
+    // Event listeners for color sliders
+    redSlider.addEventListener("input", updateCubeColor);
+    greenSlider.addEventListener("input", updateCubeColor);
+    blueSlider.addEventListener("input", updateCubeColor);
+
+    // Event listener for generating random code
+    generateButton.addEventListener("click", function () {
+        const randomCode = generateRandomCode();
+        generatedCode.textContent = randomCode;
+    });
+
+    // Function to generate random code
+    function generateRandomCode() {
+        // Replace this with your code generation logic
+        const code = `// Your generated code:
+        function myFunction() {
+            console.log("Hello, EMERSA!");
+        }
+        myFunction();`;
+        
+        return code;
+    }
 });
